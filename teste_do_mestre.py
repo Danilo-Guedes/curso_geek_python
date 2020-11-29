3f3c1acf5949b4cc5e99cc0c059e191b08da33c0
Danilo Guedes <danilotiba@gmail.com>
2020-11-29 19:36:51 -0300

    enviando os arquivos do curso de pytjon essencial da geek university para o repositório do GitHub

---
+++ teste_do_mestre.py
@@ -0,0 +1,12 @@
+print('Olá Danilo')
+
+if __name__ == '__main__':
+
+    def soma(a, b):
+        return a + b
+    print(soma(10, 10))
+
+    def reverencia_mestre(nome: str) -> str:
+        return f'Olá {nome}, você é e sempre será o mestre dos mestres!'
+
+    print(reverencia_mestre('Danilo Guedes'))
